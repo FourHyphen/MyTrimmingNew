04caa46c4402ec00e8ad58f901535b474eaafd0f
FourHyphen <FourHyphen.4@gmail.com>
2019-12-14 16:50:31 +0900

    TestNotEqualOfImageNameAndSaveNameExample() 追加

--- TestMyTrimmingNew/UnitTest1.cs
+++ TestMyTrimmingNew/UnitTest1.cs
@@ -5,14 +5,20 @@ namespace TestMyTrimmingNew
     [TestClass]
     public class UnitTest1
     {
-        //private const string test001JpgPath = "../../../TestMyTrimmingNew/Resources/test001.jpg";
-
         [TestMethod]
         [DeploymentItem(@".\Resources\test001.jpg")]
         public void TestSuccessOfCreateBitmapAfterNewImage()
         {
             MyTrimmingNew.Image img = new MyTrimmingNew.Image(@".\Resources\test001.jpg");
             Assert.IsNotNull(img.BitmapImage);
+        }
+
+        [TestMethod]
+        [DeploymentItem(@".\Resources\test001.jpg")]
+        public void TestNotEqualOfImageNameAndSaveNameExample()
+        {
+            MyTrimmingNew.Image img = new MyTrimmingNew.Image(@".\Resources\test001.jpg");
+            Assert.AreNotEqual(img.SaveNameExample, "test001.jpg");
         }
     }
 }
