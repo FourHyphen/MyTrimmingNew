b39f11435a78937fa03e9652ce2d7a4f624748a4
FourHyphen <FourHyphen.4@gmail.com>
2020-02-03 09:12:07 +0900

    キー操作でのUndoのテストを追加

--- TestMyTrimmingNew/UnitTestFunctionUndoRedoAuxiliaryOperation.cs
+++ TestMyTrimmingNew/UnitTestFunctionUndoRedoAuxiliaryOperation.cs
@@ -38,5 +38,60 @@ namespace TestMyTrimmingNew
             Assert.AreEqual(0, ac.AuxiliaryTopRelativeImage);
             Assert.AreEqual(0, ac.AuxiliaryLeftRelativeImage);
         }
+
+        [TestMethod]
+        [DeploymentItem(@".\Resource\test001.jpg")]
+        public void TestUndoInputCursorKeyUp()
+        {
+            AuxiliaryController ac = Common.GetAuxiliaryControllerImage001RatioTypeW16H9();
+
+            ac.SetEvent();
+            ac.PublishEvent(Keys.EnableKeys.Up);
+            Assert.AreEqual(0, ac.AuxiliaryTopRelativeImage);
+            Assert.AreEqual(0, ac.AuxiliaryLeftRelativeImage);
+
+            ac.CancelEvent();
+            Assert.AreEqual(0, ac.AuxiliaryTopRelativeImage);
+            Assert.AreEqual(0, ac.AuxiliaryLeftRelativeImage);
+        }
+
+        [TestMethod]
+        [DeploymentItem(@".\Resource\test001.jpg")]
+        public void TestUndoInputCursorKeyLeft()
+        {
+            AuxiliaryController ac = Common.GetAuxiliaryControllerImage001RatioTypeW16H9();
+
+            ac.SetEvent();
+            ac.PublishEvent(Keys.EnableKeys.Left);
+            Assert.AreEqual(0, ac.AuxiliaryTopRelativeImage);
+            Assert.AreEqual(0, ac.AuxiliaryLeftRelativeImage);
+
+            ac.CancelEvent();
+            Assert.AreEqual(0, ac.AuxiliaryTopRelativeImage);
+            Assert.AreEqual(0, ac.AuxiliaryLeftRelativeImage);
+        }
+
+        [TestMethod]
+        [DeploymentItem(@".\Resource\test001.jpg")]
+        public void TestUndoInputCursorKeyRight()
+        {
+            AuxiliaryController ac = Common.GetAuxiliaryControllerImage001RatioTypeW16H9();
+
+            // 矩形を小さくして、右キーで矩形を移動できるだけのスペースを作る
+            AuxiliaryLineTestData testData
+                = new AuxiliaryLineChangeSizeBottomRight().ChangeSize(ac,
+                                                                      -100,
+                                                                      -5,
+                                                                      true);
+
+            ac.SetEvent();
+            ac.PublishEvent(Keys.EnableKeys.Right);
+            Assert.AreEqual(0, ac.AuxiliaryTopRelativeImage);
+            Assert.AreEqual(1, ac.AuxiliaryLeftRelativeImage);
+
+            ac.CancelEvent();
+            Assert.AreEqual(0, ac.AuxiliaryTopRelativeImage);
+            Assert.AreEqual(0, ac.AuxiliaryLeftRelativeImage);
+        }
     }
 }
